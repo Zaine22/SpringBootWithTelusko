d01d771aff78303d1df111631f53528e1ced3627
Zaine <kyawkokozin223@gmail.com>
2025-01-14 10:53:02 +0630

    first spring boot with telusko

---
+++ src/main/java/com/zaine/myapp/Dev.java
@@ -0,0 +1,11 @@
+package com.zaine.myapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("working on Awesome Project");
+    }
+}
